14a007063adec466e20bda86b315475d48b42e54
k2ffilms-hub <k2ffilms@gmail.com>
2025-10-14 01:10:01 +0000

    editando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main(){
+  int carta = 1;
+  int populacao = 12.325000;
+  int pontos turisticos = 50;
+  char estado = 'A';
+  char codigo da carta [50]= A1;
+  char nome[20] = Saopaulo;
+  char nome da cidade = saopaulo;
+  float area = 1521.11 km;
+  float pib = 699.28 bilhoes;
 
-  printf("Desafio Xadres!\n");
+
+  printf("Carta de Trunfo!\n");
+  printf("A populacao do %s é: %d\n" )
 
 } 
