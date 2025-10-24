828eec106612b3e062351c0f307aeb7149e034e5
k2ffilms-hub <k2ffilms@gmail.com>
2025-10-24 11:36:16 +0000

    carta 2 prata

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 int main(){
-  char tipo [10] = "Ouro";
-  int codigo = 1;
-  float populacao = 12.325; 
-  int turisticos = 50;
-  char estado [10] = "SP";
-  char carta [10] = "A01";
-  char cidade [20] = "Sao paulo";
-  float area = 1521.11;
-  float pib = 699.28;
+  char tipo [10] = "Prata";
+  int codigo = 2;
+  float populacao = 6.74800; 
+  int turisticos = 30;
+  char estado [10] = "RJ";
+  char carta [10] = "B02";
+  char cidade [20] = "Rio de janeiro";
+  float area = 1200.25;
+  float pib = 300.50;
 
   printf("Tipo da carta: %s.\n",tipo);
   printf("Estado: %s.\n",estado);
   printf("Codigo da carta: %s.\n",carta);
   printf("Cidade: %s.\n",cidade);
-  printf("Populacao: %f.\n",populacao);
+  printf("Populacao: %.6f.\n",populacao);
   printf("Area: %.2f.\n",area);
-  printf("PIB: %f.\n",pib);
+  printf("PIB: %.2f.\n",pib);
   printf("Pontos Turisticos: %d.\n",turisticos);
   
 }
